e1e2284fe23279bebd24603a5c139f43639082d7
hyvnsevng <yh189487@gmail.com>
2024-08-22 21:57:57 +0900

    백준 이진탐색 풀이

--- BOJ/1715.py
+++ BOJ/1715.py
@@ -1,7 +1,5 @@
-# 미완
-
 import sys
-from collections import deque
+import heapq
 
 N = int(input())
 num_list = []
@@ -9,26 +7,14 @@ num_list = []
 for _ in range(N):
     num_list.append(int(sys.stdin.readline()))
 
-
-num_list.sort()
-queue = deque(num_list)
-
+heapq.heapify(num_list)
 ans = 0
 
-while len(queue) > 2:
-    if queue[0] + queue[1] < queue[1] + queue[2]:
-        queue[0] = queue.popleft()+queue[0]
-        ans += queue[0]
+for i in range(N-1):
+    num1 = heapq.heappop(num_list)
+    num2 = heapq.heappop(num_list)
+    min_sum = num1 + num2
+    ans += min_sum
+    heapq.heappush(num_list, min_sum)
 
-    else: 
-        first_index = queue.popleft()
-        queue[1] = queue[0] + queue[1]
-        queue[0] = first_index
-        ans += queue[1]
-
-
-print(ans+sum(queue))
-
-
-# 3 3 3 5 7
-# 3+3+8+14+21
+print(ans)
