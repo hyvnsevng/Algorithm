1d4dc47007f16c4e46e9d69afe7f0dc5eef5a7f5
hyvnsevng <yh189487@gmail.com>
2024-12-15 22:53:19 +0900

    백준 154486번 퇴사 2 주석 추가

--- BOJ/BOJ_15486.py
+++ BOJ/BOJ_15486.py
@@ -13,11 +13,13 @@ for _ in range(n):
     t, p = map(int, sys.stdin.readline().split())
     table.append([t, p])
 
-
+# 마지막 날의 일정부터 체크
 for day in range(n-1, -1, -1):
     t, p = table[day]
-    if day + t <= n and p + dp[day] > dp[day + t]:
-        for i in range(day + t, n):
-            dp[i] = dp[day] + p
 
-print(dp)
+    if day + t <= n:    # 상담 일정이 퇴사 전에 끝난다면
+        dp[day] = max(dp[day + t] + p, dp[day + 1])     # DP테이블을 당일값 + p와 익일값 중 큰 값으로 갱신
+    else:
+        dp[day] = dp[day+1]
+
+print(max(dp))
