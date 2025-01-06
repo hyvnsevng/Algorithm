53d469cf7e6ee7bbd80fd0185b37f5af15f7954c
hyvnsevng <yh189487@gmail.com>
2025-01-06 23:45:48 +0900

    [Java] 백준 3460번 이진수

---
+++ Java/BOJ_3460/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 0; i < t; i++) {
+            int n = sc.nextInt();
+            int idx = 0;
+            while (n > 0) {
+                if (n % 2 == 1) {
+                    System.out.print(idx + " ");
+                }
+                idx++;
+                n /= 2;
+            }
+            System.out.println();
+        }
+    }
+}
