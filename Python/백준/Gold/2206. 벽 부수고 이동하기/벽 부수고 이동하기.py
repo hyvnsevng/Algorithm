21d879513482acb91328d7e45fa308e95a4f4799
hyvnsevng <yh189487@gmail.com>
2025-02-24 23:32:26 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 3900 ms, Memory: 192924 KB -BaekjoonHub

--- Python/백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ Python/백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -1,53 +1,36 @@
 from collections import deque
 
 n, m = map(int, input().split())
-graph = [list(map(int, input())) for _ in range(n)]
+matrix = [list(map(int, input())) for _ in range(n)]
 
-visited = [[[1e9, 1e9] for _ in range(m)] for _ in range(n)]
-dcol, drow = [1, -1, 0, 0], [0, 0, 1, -1]
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
 
-minDist = 1e9  # row, col, dist
+def bfs(start):
+    sx, sy = start
+    queue = deque()
+    queue.append((sx, sy, 0))
+    visited = [[[0] * 2 for _ in range(m)] for _ in range(n)]
+    visited[sx][sy][0] = 1
+    
+    while queue:
+        tx, ty, broken = queue.popleft()
 
-q = deque()
-q.append([0, 0, 1, False])  # row, col, dist, isBroken
+        if tx == n - 1 and ty == m - 1:
+            return visited[n - 1][m - 1][broken]
+        
+        for k in range(4):
+            nx = tx + dx[k]
+            ny = ty + dy[k]
+                
+            if 0 <= nx < n and 0 <= ny < m:
+                if matrix[nx][ny] == 0 and visited[nx][ny][broken] == 0:
+                    visited[nx][ny][broken] = visited[tx][ty][broken] + 1
+                    queue.append((nx, ny, broken))
+                elif matrix[nx][ny] == 1 and broken == 0:
+                    visited[nx][ny][1] = visited[tx][ty][broken] + 1
+                    queue.append((nx, ny, 1))
 
-# visited에 거리를 저장. -> 이전 거리보다 짧으면 걍 궈궈한다.
-# 벽 부쉈을 때 방문 처리 어떻게 할까?
-# 방문처리를 거리순으로 하면, 만약 벽을 부수고 더 짧은 거리 vs 벽을 안 부수고 더 먼 거리 인데? 지금 벽을 안 부수고 더 먼 거리
-# 방문처리: visited 배열에 거리와 벽 부순 여부.
-while q:
-    # 큐에서 값 꺼내기
-    row, col, dist, isBroken = q.popleft()
+    return -1
 
-    # 최단거리 갱신
-    if row == n-1 and col == m-1 and dist < minDist:
-        minDist = dist
-
-    # 방문처리: 벽 부순 경우
-    if isBroken and visited[row][col][1] > dist:
-        visited[row][col][1] = dist
-    # 방문처리: 벽 안 부순 경우
-    elif not isBroken and visited[row][col][0] > dist:
-        visited[row][col] = [dist, dist]
-
-    for i in range(4):
-        ncol, nrow = col + dcol[i], row + drow[i]
-        if 0 <= ncol < m and 0 <= nrow < n:
-            # 벽 이미 부순 경우
-            if isBroken and not graph[nrow][ncol] and dist + 1 < visited[nrow][ncol][1]:
-                q.append([nrow, ncol, dist + 1, True])
-                visited[nrow][ncol][1] = dist
-
-            # 벽 아직 안 부순 경우
-            elif not isBroken:
-                # 부술 경우
-                if dist + 1 < visited[nrow][ncol][1] and graph[nrow][ncol]:
-                    q.append([nrow, ncol, dist + 1, True])
-                    visited[nrow][ncol][1] = dist
-                # 안 부술 경우
-                elif dist + 1 < visited[nrow][ncol][0] and not graph[nrow][ncol]:
-                    q.append([nrow, ncol, dist + 1, False])
-                    visited[nrow][ncol] = [dist, dist]
-
-
-print(-1 if minDist == 1e9 else minDist)
+print(bfs((0, 0)))
