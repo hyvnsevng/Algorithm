d4facb7ffe73ff8f0f4fc9aa94fdd2a44a65ac4d
hyvnsevng <yh189487@gmail.com>
2025-03-03 21:21:24 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 3576 ms, Memory: 191700 KB -BaekjoonHub

--- Python/백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
+++ Python/백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.py
@@ -1,3 +1,15 @@
+def binary_search(arr, target):
+    s, e = 0, len(arr) - 1
+    while s <= e:
+        mid = (s + e) // 2
+        if arr[mid] == target:
+            return mid
+        if arr[mid] < target:
+            s = mid + 1
+        else:
+            e = mid - 1
+    return s
+
 n = int(input())
 numList = list(map(int, input().split()))
 v = numList[0:1]
@@ -8,16 +20,9 @@ for num in numList:
         v.append(num)
         backtrace.append((len(v) - 1, num))
     else:
-        start, end = 0, len(v)-1
-        while start < end:
-            mid = (start + end) // 2
-            if v[(start + end) // 2] < num:
-                start = mid + 1
-            else:
-                end = mid
-
-        v[end] = num
-        backtrace.append((end, num))
+        idx = binary_search(v, num)
+        v[idx] = num
+        backtrace.append((idx, num))
 
 idx = len(v) - 1
 
@@ -29,6 +34,7 @@ while backtrace:
 
     if idx < 0:
         break
+
 
 print(len(v))
 for elem in v:
