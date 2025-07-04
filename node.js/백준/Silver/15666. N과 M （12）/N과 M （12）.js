c45e06ce6e0d187375fc1d2d237501dfccdbf1fe
hyvnsevng <yh189487@gmail.com>
2025-07-04 22:28:57 +0900

    [Silver II] Title: N과 M (12), Time: 288 ms, Memory: 15988 KB -BaekjoonHub

---
+++ node.js/백준/Silver/15666. N과 M （12）/N과 M （12）.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs
+  .readFileSync("/dev/stdin")
+  .toString()
+  .split("\n");
+
+let idx = 0;
+
+const [n, m] = input[idx++].split(" ").map((e) => parseInt(e));
+
+const nums = [
+  ...new Set(
+    input[idx++]
+      .split(" ")
+      .map((e) => parseInt(e))
+      .sort((a, b) => a - b)
+  ),
+];
+
+const answer = [];
+function solution(curr, depth) {
+  if (depth === m) {
+    return answer.push(curr);
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (!curr.length || nums[i] >= curr[curr.length - 1]) {
+      solution([...curr, nums[i]], depth + 1);
+    }
+  }
+}
+
+solution([], 0);
+answer.forEach((comb) => console.log(...comb));
