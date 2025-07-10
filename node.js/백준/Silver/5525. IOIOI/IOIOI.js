224e80f3bd7d902cfceb29c4fb9d553537a0abf4
hyvnsevng <yh189487@gmail.com>
2025-07-11 00:27:06 +0900

    [Silver I] Title: IOIOI, Time: 208 ms, Memory: 15152 KB, Score: 100 point -BaekjoonHub

--- node.js/백준/Silver/5525. IOIOI/IOIOI.js
+++ node.js/백준/Silver/5525. IOIOI/IOIOI.js
@@ -9,46 +9,35 @@ const [N, M] = input.slice(0, 2).map((e) => Number(e));
 const S = input[2];
 
 let answer = 0;
-// let start = false;
-// let prev = "";
-// let len = 0;
+let start = false;
+let prev = "";
+let len = 0;
 
-const P = "IO".repeat(N) + "I";
-
-// for (const letter of S) {
-//   // I
-//   if (letter === "I") {
-//     if (!start) {
-//       start = true;
-//     } else {
-//       if (prev === "O") {
-//         ++len;
-//         if (len >= N) {
-//           answer++;
-//         }
-//       } else {
-//         len = 0;
-//       }
-//     }
-//     prev = "I";
-//   } else {
-//     // O
-//     if (start && prev === "O") {
-//       answer += len - N + 1;
-//       start = false;
-//       len = 0;
-//     }
-//     prev = "O";
-//   }
-//   //   console.log(letter, start, prev, len, answer);
-// }
-
-for (let i = 0; i < M - N + 1; i++) {
-  if (S.slice(i, i + 1) === "I") {
-    if (S.slice(i, i + P.length) === P) {
-      answer++;
+for (const letter of S) {
+  // I
+  if (letter === "I") {
+    if (!start) {
+      start = true;
+    } else {
+      if (prev === "O") {
+        ++len;
+        if (len >= N) {
+          answer++;
+        }
+      } else {
+        len = 0;
+      }
     }
+    prev = "I";
+  } else {
+    // O
+    if (prev === "O") {
+      start = false;
+      len = 0;
+    }
+    prev = "O";
   }
+  //   console.log(letter, start, prev, len, answer);
 }
 
 console.log(answer);
