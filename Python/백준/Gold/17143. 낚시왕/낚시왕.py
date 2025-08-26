27677bb4e0213be405ebe38f0a5bfefc92881c58
hyvnsevng <yh189487@gmail.com>
2025-08-26 23:06:31 +0900

    [Gold I] Title: 낚시왕, Time: 1956 ms, Memory: 36504 KB -BaekjoonHub

--- Python/백준/Gold/17143. 낚시왕/낚시왕.py
+++ Python/백준/Gold/17143. 낚시왕/낚시왕.py
@@ -1,6 +1,10 @@
+import sys
+
+input = sys.stdin.readline
+
 R, C, M = map(int, input().split())
-sharks = dict()
-graph = [[[] for _ in range(C)] for _ in range(R)]
+sharks = dict()     # key: 상어 크기 / value: [열, 행, 속력, 방향]
+graph = [[[] for _ in range(C)] for _ in range(R)]      # 격자판 -> 각 격자의 원소: 상어 크기 배열
 
 for _ in range(M):
     r, c, s, d, z = map(int, input().split())
@@ -31,8 +35,8 @@ def move_shark(r, c, s, d):
         move = -s
 
     next, nd = nextLoc(curr, move, limit, d)
-    coor[d // 3] = next
-    return [*coor, nd]
+    coor[d // 3] = next # 1, 2 -> 열 이동 / 3, 4 -> 행 이동
+    return [*coor, nd]  # 새로운 위치와 방향 return
 
 
 def nextLoc(curr, move, limit, d):
@@ -48,35 +52,35 @@ def nextLoc(curr, move, limit, d):
 
 answer = 0
 for col in range(1, C + 1):
-    captured = [R + 1, 0, 0, 0, 0]  # r, c, s, d, z
-    for z in sharks:
-        r, c, s, d = sharks[z]
-        if c == col and captured[0] > r:
-            captured = [r, c, s, d, z]
-            
-    if captured[4]:
-        answer += captured[4]
-        del sharks[captured[4]]
-        graph[captured[0] - 1][captured[1] - 1] = []
+    # 가장 가까운 행의 상어 잡기
+    for row in range(R):
+        if graph[row][col - 1]:
+            key = graph[row][col - 1][0]
+            del sharks[key]
+            answer += key
+            graph[row][col - 1] = []
+            break
 
-    del_list = []
+    # 상어 이동
+    del_list = []   # 둘 이상의 상어가 있는 위치를 저장
     for z in sharks:
         r, c, s, d = sharks[z]
         nr, nc, nd = move_shark(r, c, s, d)
         node = graph[nr - 1][nc - 1]
         
+        # 현재 칸에 이미 상어가 있다면 del_list에 위치 추가
         if node:
             del_list.append((nr, nc))
         graph[r - 1][c - 1].remove(z)
         graph[nr - 1][nc - 1].append(z)
         sharks[z] = [nr, nc, s, nd]
 
+    # 같은 위치에 있는 상어 처리하기 
     for r, c in del_list:
-        max_size = max(graph[r - 1][c - 1])
+        max_size = max(graph[r - 1][c - 1])     # 가장 큰 상어의 크기
         for shark in graph[r - 1][c - 1]:
-            if shark < max_size: del sharks[shark]
-        graph[r - 1][c - 1] = [max_size]
-                
+            if shark < max_size: del sharks[shark]  # 작은 애들 다 잡아먹는다
+        
+        graph[r - 1][c - 1] = [max_size]    # graph 갱신
     
 print(answer)
-    
