fa1db617658b2d1cebf9f2e7dd47e9d641a50729
hyvnsevng <yh189487@gmail.com>
2025-10-22 18:13:56 +0900

    [Gold I] Title: 동전 뒤집기, Time: 3516 ms, Memory: 9668 KB -BaekjoonHub

--- node.js/백준/Gold/1285. 동전 뒤집기/동전 뒤집기.js
+++ node.js/백준/Gold/1285. 동전 뒤집기/동전 뒤집기.js
@@ -6,20 +6,22 @@ const n = Number(input[0]);
 const coins = input.slice(1).map((e) => e.split(""));
 const status = ["H", "T"];
 
-let answer = n ** 2;
+let answer = n ** 2,
+  tmp = 0,
+  count = 0;
+const cols = Array(n);
 
 for (let colMask = 0; colMask < 1 << n; ++colMask) {
   // 열 뒤집는 경우의 수
-  const cols = Array(n);
   for (let i = 0; i < n; i++) {
     cols[i] = colMask & (1 << i) ? 1 : 0;
   }
 
-  let count = 0; // 이 케이스에서의 최소 개수
+  count = 0; // 이 케이스에서의 최소 개수
 
   for (let row = 0; row < n; ++row) {
     // 행 별로 최소개수 구하기
-    let tmp = 0;
+    tmp = 0;
     for (let col = 0; col < n; ++col) {
       // 열 뒤집은 것 판단해서 개수 더하기
       if (coins[row][col] == status[cols[col]]) {
