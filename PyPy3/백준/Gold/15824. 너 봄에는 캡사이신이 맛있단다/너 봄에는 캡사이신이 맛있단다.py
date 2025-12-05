6817f95ed3a596ab0764fafa0a0fed4ef0465a6c
hyvnsevng <yh189487@gmail.com>
2025-12-05 22:47:34 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 228 ms, Memory: 169216 KB, Score: 250 point -BaekjoonHub

--- PyPy3/백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
+++ PyPy3/백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.py
@@ -1,14 +1,19 @@
-DIVIDE = 1000000007
+MOD = 1000000007
 
 n = int(input())
 spicy = list(map(int, input().split()))
 spicy.sort()
+POWER = [0]*n
+
+for i in range(1, n):
+    POWER[i] = (2 * POWER[i-1] + 1) % MOD
 
 ans = 0
-for interval in range(1, n):    # 길이
-    tmp = 0
-    for start in range(n-interval):    # 시작 idx
-        tmp += spicy[start+interval] - spicy[start]
-    ans += tmp * 2**(interval-1)
+for i in range(n):
+    cnt = POWER[i] - POWER[n-i-1]
+    if cnt < 0:
+        cnt += MOD
+    added = spicy[i] * cnt
+    ans += added
 
-print(ans % DIVIDE)
+print(ans % MOD)
