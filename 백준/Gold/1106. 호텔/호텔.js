8a3ce023da940474ca0c876c065dd2a4bccce03c
hyvnsevng <yh189487@gmail.com>
2025-12-19 13:22:00 +0900

    [Gold IV] Title: 호텔, Time: 96 ms, Memory: 9372 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.js
+++ 백준/Gold/1106. 호텔/호텔.js
@@ -7,20 +7,19 @@ const input = fs
   .split("\n")
   .map((line) => line.split(" ").map((n) => Number(n)));
 
-const [C, N] = input[0];
-const promos = [];
-const dp = Array(2001).fill(200000); // idx명 유치 최소 비용
+const [C, N] = input.shift();
+const maxNum = Math.max(...input.map((e) => e[1]));
+const maxIdx = C + maxNum;
 
-for (let i = 1; i < N + 1; ++i) {
-  const [a, b] = input[i]; // [비용, 고객 수]
-  promos.push([a, b]);
+const dp = Array(maxIdx + 1).fill(100000); // idx명 유치 최소 비용
+
+for (const [a, b] of input) {
   dp[b] = Math.min(dp[b], a);
 }
 
-for (let i = 1; i < 2001; ++i) {
-  for (const [cost, customers] of promos) {
-    if (i + customers > 2000) continue;
-    dp[i + customers] = Math.min(dp[i + customers], dp[i] + cost);
+for (const [cost, customers] of input) {
+  for (let i = customers; i < maxIdx; ++i) {
+    dp[i] = Math.min(dp[i], dp[i - customers] + cost);
   }
 }
 
