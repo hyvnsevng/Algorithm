0e5d91ebf273f88efabd9492c29dd49067fc301c
hyvnsevng <yh189487@gmail.com>
2025-12-21 00:30:49 +0900

    [Silver I] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.py
+++ 백준/Silver/1629. 곱셈/곱셈.py
@@ -1,13 +1,20 @@
+def mod(n, c):
+    return n % c
+
+
+def pow(n, k, c):
+    if k == 0:
+        return 1
+    elif k == 1:
+        return mod(n, c)
+    else:
+        x = mod(pow(n, k // 2, c), c)
+        
+        if k % 2:
+            return mod(mod(x * x, c) * mod(a, c), c)
+        else:
+            return mod(x * x, c)
+
+
 a, b, c = map(int, input().split())
-st = [-1] * 32
-st[0] = a % c
-for i in range(1, 32):
-    st[i] = (st[i-1] * st[i-1]) % c
-
-ans = 1
-
-for j in range(32):
-    if b & 1 << j:
-        ans *= st[j]
-    
-print(ans % c)
+print(pow(a, b, c))
