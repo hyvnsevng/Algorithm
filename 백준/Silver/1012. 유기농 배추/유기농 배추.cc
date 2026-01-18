2d3612c9e27800f35cc017aff84dd7c3e1a71244
hyvnsevng <yh189487@gmail.com>
2026-01-18 23:42:08 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -2,45 +2,36 @@
 
 using namespace std;
 
-int T, M, N, K;
-vector<vector<int>> baechu;
+int M, N, K;
+int baechu[51][51];
 int dr[4] = {1, 0, -1, 0};
 int dc[4] = {0, 1, 0, -1};
 
 int dfs(int r, int c) {
+    baechu[r][c] = 0;
     int nr, nc;
     for (int i = 0; i < 4; ++i) {
         nr = r + dr[i]; nc = c + dc[i];
         if (nr >= 0 && nr < N && nc >= 0 && nc < M && baechu[nr][nc] == 1) {
-            baechu[nr][nc] = 0;
             dfs(nr, nc);
         }
     }
-    return 0;
-}
-
-int func(int r, int c) {
-    if (!baechu[r][c]) {
-        return 0;
-    }
-
-    dfs(r, c);
+    
     return 1;
 }
 
 int solve(int M, int N, int K) {
     int r, c;
-    vector<pair<int, int>> worms;
-    baechu.assign(N, vector<int> (M, 0));
     for (int i = 0; i < K; ++i) {
         cin >> c >> r;
-        worms.push_back({r, c});
         baechu[r][c] = 1;
     }
     
     int cnt=0;
-    for (auto loc : worms) {
-        cnt += func(loc.first, loc.second);
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+            if (baechu[i][j]) cnt += dfs(i, j);
+        }
     }
 
     return cnt;
@@ -49,10 +40,10 @@ int solve(int M, int N, int K) {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    int tc; cin >> tc;
     
-    cin >> T;
-    
-    for (int tc=0; tc < T; tc++) {
+    while (tc--) {
         cin >> M >> N >> K;
         cout << solve(M, N, K) << "\n";
     }
