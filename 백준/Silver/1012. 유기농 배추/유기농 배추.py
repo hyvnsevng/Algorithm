8273ac346c229d3081c0956e0cfd7676e2bdb29b
hyvnsevng <yh189487@gmail.com>
2026-01-19 00:09:55 +0900

    [Silver II] Title: 유기농 배추, Time: 204 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,27 @@
+dr = [1, 0, -1, 0]
+dc = [0, 1, 0, -1]
+T = int(input())
+for t in range(T):
+    m, n, k = map(int, input().split())
+    baechu = [[0 for _ in range(m)] for _ in range(n)]
+    for __ in range(k):
+        c, r = map(int, input().split())
+        baechu[r][c] = 1
+        
+    cnt = 0
+    stack = []
+    for i in range(n):
+        for j in range(m):
+            if baechu[i][j]:
+                stack.append((i, j))
+                while stack:
+                    r, c = stack.pop()
+                    for k in range(4):
+                        nr = r + dr[k]
+                        nc = c + dc[k]
+                        if 0 <= nr < n and 0 <= nc < m and baechu[nr][nc]:
+                            baechu[nr][nc] = 0
+                            stack.append((nr, nc))
+                cnt += 1
+    
+    print(cnt)
