ce976a2836b526a82b0d6f9164445e5eb6263814
hyvnsevng <yh189487@gmail.com>
2026-01-21 00:47:14 +0900

    [Gold IV] Title: Σ, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.cc
+++ 백준/Gold/13172. Σ/Σ.cc
@@ -8,11 +8,13 @@ ll mod(ll b, ll e) {
     if (e == 1) return b % X;
 
     ll m = mod(b, e / 2);
-    if (e % 2) return (((m * m) % X) * b) % X;
+    if (e & 1) return (((m * m) % X) * b) % X;
     else return (m * m) % X;
 }
 
 int main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0);
     int tc;
     cin >> tc;
     
