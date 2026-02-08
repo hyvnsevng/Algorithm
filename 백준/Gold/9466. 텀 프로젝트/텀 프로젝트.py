42e5b013bd80b8c45c797608833789303f0cca81
hyvnsevng <yh189487@gmail.com>
2026-02-08 15:32:51 +0900

    [Gold III] Title: 텀 프로젝트, Time: 1936 ms, Memory: 50008 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,31 +1,24 @@
 import sys
-sys.setrecursionlimit(1000000)
+from collections import deque
 input = sys.stdin.readline
-
-def dfs(node):
-    if visited[node] == 1:
-        nxt = students[node]
-        res = 0
-        while nxt != node:
-            res += 1
-            nxt = students[nxt]
-        return res + 1
-    if visited[node] == 2:
-        return 0
-
-    visited[node] = 1
-    r = dfs(students[node])
-    visited[node] = 2
-    return r
-
-
 
 T = int(input())
 for tc in range(T):
     n = int(input())
     students = [0] + list(map(int, input().split()))
-    visited = [0 for _ in range(n+1)]
-    ans = 0
+    indegree = [0 for _ in range(n+1)]
     for i in range(1, n+1):
-        if not visited[i]: ans += dfs(i)
-    print(n - ans)
+        indegree[students[i]] += 1
+    q = deque()
+    for i in range(1, n+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        curr = q.popleft()
+        nxt = students[curr]
+        indegree[nxt] -= 1
+        if indegree[nxt] == 0:
+            q.append(nxt)
+
+    print(n-sum(indegree))
