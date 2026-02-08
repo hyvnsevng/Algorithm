a2a37e61198be21fea2900e909e45942fe0b0f48
hyvnsevng <yh189487@gmail.com>
2026-02-08 23:54:24 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 2224 ms, Memory: 48856 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.py
@@ -2,21 +2,18 @@ n = int(input())
 arr = list(map(int, input().split()))
 max_num = max(arr)
 
-locations = [1000001 for _ in range(max_num +1)]
+locations = dict()
 for i in range(n):
     locations[arr[i]] = i
 
-arr.sort()
-
-nums = set(arr)
 score = [0 for _ in range(n)]
 
-gcd = [[] for _ in range(max_num + 1)]
 for i in arr:
     k = 2*i
     while k <= max_num:
-        if k in nums:
-            score[locations[k]] -= 1
+        find = locations.get(k, -1)
+        if find >= 0:
+            score[find] -= 1
             score[locations[i]] += 1 
         k += i
 
