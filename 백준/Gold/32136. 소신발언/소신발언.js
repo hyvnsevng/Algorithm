e26acf1bf506824d4cd260a51466f13ed02c8adf
hyvnsevng <yh189487@gmail.com>
2026-02-12 00:03:57 +0900

    [Gold I] Title: 소신발언, Time: 5736 ms, Memory: 66892 KB -BaekjoonHub

---
+++ 백준/Gold/32136. 소신발언/소신발언.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
+
+const N = Number(input[0]);
+const arr = input[1].split(" ").map((e) => Number(e));
+
+function check(arr, t) {
+  const N = arr.length;
+  let [mini, maxi] = [0n, BigInt(N)];
+  for (let j = 0; j < N; ++j) {
+    const tmini = -1n * (t / BigInt(arr[j])) + BigInt(j);
+    const tmaxi = t / BigInt(arr[j]) + BigInt(j);
+    mini = mini < tmini ? tmini : mini;
+    maxi = maxi > tmaxi ? tmaxi : maxi;
+    if (mini > maxi) return false;
+  }
+  return true;
+}
+
+let [l, r] = [1n, 150000000000n];
+while (l < r) {
+  const m = (l + r) >> 1n;
+  if (check(arr, m)) r = m;
+  else l = m + 1n;
+}
+
+console.log(String(l));
