44a136a4ec0c3b66ac03f0ec49d6ee57ce6c9b80
hyvnsevng <yh189487@gmail.com>
2026-02-12 00:30:31 +0900

    [Gold I] Title: 소신발언, Time: 336 ms, Memory: 40504 KB -BaekjoonHub

--- 백준/Gold/32136. 소신발언/소신발언.js
+++ 백준/Gold/32136. 소신발언/소신발언.js
@@ -4,29 +4,26 @@ const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 const N = Number(input[0]);
 const arr = input[1].split(" ").map((e) => Number(e));
 
-function check(arr, t) {
-  const N = arr.length;
-  let [mini, maxi] = [0n, BigInt(N - 1)];
+function check(t) {
+  let [lo, hi] = [0, N - 1];
   for (let j = 0; j < N; ++j) {
-    const tmini = -1n * (t / BigInt(arr[j])) + BigInt(j);
-    const tmaxi = t / BigInt(arr[j]) + BigInt(j);
-    mini = mini < tmini ? tmini : mini;
-    maxi = maxi > tmaxi ? tmaxi : maxi;
-    if (mini > maxi) return false;
+    const d = Math.floor(t / arr[j]);
+    const tl = j - d;
+    const th = j + d;
+    lo = lo < tl ? tl : lo;
+    hi = hi > th ? th : hi;
+    if (lo > hi) return false;
   }
   return true;
 }
 
-let maxA = 0;
-for (let i = 0; i < N; ++i) {
-  maxA = Math.max(maxA, (N - 1 - i) * arr[i], i * arr[i]);
+const maxA = Math.max(...arr);
+
+let [l, r] = [-1, maxA * (N - 1)];
+while (l + 1 < r) {
+  const m = Math.floor((l + r) / 2);
+  if (check(m)) r = m;
+  else l = m;
 }
 
-let [l, r] = [1n, BigInt(maxA)];
-while (l < r) {
-  const m = (l + r) >> 1n;
-  if (check(arr, m)) r = m;
-  else l = m + 1n;
-}
-
-console.log(String(l));
+console.log(r);
