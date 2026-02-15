e7691acbd7da72c441a850c784bfb5748830d035
hyvnsevng <yh189487@gmail.com>
2026-02-16 01:00:51 +0900

    [Gold II] Title: 색종이 붙이기, Time: 1992 ms, Memory: 32424 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.py
@@ -12,38 +12,37 @@ def backtrack(r, c, cnt, use, ans):
     if graph[r][c] == 0:
         return backtrack(r, c+1, cnt, use, ans)
     
-    for i in range(4, -1, -1):
-        k = flip(r, c, i+1, use)
-        if not k:
+    max_size = get_max_size(r, c)
+    for size in range(max_size, -1, -1):
+        if use[size] == 0:
             continue
-        use[i] -= 1
+        flip(r, c, size+1, 0)
+        use[size] -= 1
         res = backtrack(r, c+1, cnt+1, use, ans)
         if res > 0:
             ans = res
-        use[i] += 1
-        reflip(r, c, r+i+1, c+i+1, i+1)
+        use[size] += 1
+        flip(r, c, size+1, 1)
     
     return ans if ans < 25 else -1
 
 
-def flip(r, c, size, use):
-    if use[size-1] == 0 or r+size > 10 or c+size > 10:
-        return False
+def get_max_size(r, c):
+    for size in range(1, 5):    # 1, 2, 3, 4
+        if r+size >= 10 or c+size >= 10:
+            return size-1
+        for diff in range(size+1):
+            if graph[r+diff][c+size] == 0:
+                return size-1
+            if graph[r+size][c+diff] == 0:
+                return size-1
+    return 4
+                
+
+def flip(r, c, size, t):
     for dr in range(size):
         for dc in range(size):
-            if graph[r+dr][c+dc] == 0:
-                reflip(r, c, r+dr, c+dc, size)
-                return False
-            graph[r+dr][c+dc] = 0
-    return True
-
-
-def reflip(r, c, lim_r, lim_c, size):
-    for dr in range(size):
-        for dc in range(size):
-            if r+dr == lim_r and c+dc == lim_c:
-                return
-            graph[r+dr][c+dc] = 1
+            graph[r+dr][c+dc] = t
 
 
 print(backtrack(0, 0, 0, [5]*5, 25))
