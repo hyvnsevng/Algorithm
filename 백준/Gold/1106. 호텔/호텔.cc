507a612ff0c41f4ef106b6c14c08a018ceec8d73
hyvnsevng <yh189487@gmail.com>
2026-03-06 22:33:38 +0900

    [Gold IV] Title: 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.cc
+++ 백준/Gold/1106. 호텔/호텔.cc
@@ -5,17 +5,16 @@ int MAX = 100000;
 int main() {
   int c, n;
   cin >> c >> n;
-  int dp[MAX+1] = {0, }; // dp[i]: i명 유치하는데 드는 최소비용
-  for (int i=1; i<=MAX; ++i) dp[i]=MAX;
+  int dp[c+1] = {0, }; // dp[i]: i명 유치하는데 드는 최소비용
+  for (int i=1; i<=c; ++i) dp[i]=MAX;
   int a, b;  // 비용, 고객의 수
   for (int _=0; _<n; ++_) {
     cin >> a >> b;
-    for (int cnt=b; cnt<=MAX; cnt++) {
-      dp[cnt]=min(dp[cnt], dp[cnt-b]+a);
+    for (int cnt=0; cnt<=c; cnt++) {
+      int i = cnt+b > c ? c : cnt+b; // 최대 인덱스 제한
+      dp[i]=min(dp[i], dp[cnt]+a);
     }
   }
 
-  int ans = MAX;
-  for (int i=c; i<=MAX; ++i) ans = min(ans, dp[i]);
-  cout << ans;
+  cout << dp[c];
 }
