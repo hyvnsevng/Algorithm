55978afe39c1146988ecf0c75917df3bc2308756
hyvnsevng <yh189487@gmail.com>
2026-03-10 01:19:00 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 292 ms, Memory: 21972 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.js
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.js
@@ -1,51 +1,108 @@
-const fs = require("fs")
-const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n")
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf-8").toString().trim().split("\n");
 
-const [N, M] = input[0].split(" ").map(e => Number(e))
-const A = input.slice(1, N+1).map(row => row.split(" ").map(e => Number(e)))
-const clouds = [[N-1, 0], [N-1, 1], [N-2, 0], [N-2, 1]]
-const dirs = [, [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]] // ←, ↖, ↑, ↗, →, ↘, ↓, ↙
-const diag = [-1, 1]
-
-for (let i=N+1; i<N+M+1; ++i) {
-    const [d, s] = input[i].split(" ").map(e => Number(e))
-    const newClouds = []
-    const isCloud = Array.from({ length: N }, () => Array(N).fill(false));
-    while (clouds.length) {
-        const [r, c] = [...clouds.pop()]    // 이동 전 구름 좌표
-        const [mr, mc] = [((r + dirs[d][0]*s) % N + N) % N, ((c + dirs[d][1]*s) % N + N) % N]   // 이동 후 구름 좌표
-        newClouds.push([mr, mc])    // 이동
-        isCloud[mr][mc] = true
-        A[mr][mc] += 1
+class Grid {
+    constructor(n, data) {
+        this.N = n;
+        this.cells = data;
     }
 
-  // 이동한 구름 좌표에서 물복사
-    for (const cloud of newClouds) {
-        let cnt = 0
-        for (const dr of diag) {
-            for (const dc of diag){
-                const [nr, nc] = [cloud[0]+dr, cloud[1]+dc]
-                if (nr >= 0 && nr < N && nc >= 0 && nc < N && A[nr][nc] > 0) {
-                    cnt++
+    // 격자 범위 내에 있는지 확인 (물복사 버그용)
+    isValid(r, c) {
+        return r >= 0 && r < this.N && c >= 0 && c < this.N;
+    }
+
+    // 물 증가
+    addWater(r, c, amount = 1) {
+        this.cells[r][c] += amount;
+    }
+
+    // 물 감소
+    consumeWater(r, c) {
+        this.cells[r][c] -= 2;
+    }
+
+    getWater(r, c) {
+        return this.cells[r][c];
+    }
+
+    getTotalWater() {
+        return this.cells.reduce((sum, row) => sum + row.reduce((a, b) => a + b, 0), 0);
+    }
+}
+
+class CloudSystem {
+    constructor(n) {
+        this.N = n;
+        // 초기 구름 설정
+        this.clouds = [[n - 1, 0], [n - 1, 1], [n - 2, 0], [n - 2, 1]];
+        this.dirs = [, [0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]];
+    }
+
+    moveAndRain(d, s, grid) {
+        const movedClouds = [];
+        const isCloudIdx = Array.from({ length: this.N }, () => Array(this.N).fill(false));
+
+        // 구름 이동 및 비 내리기
+        for (const [r, c] of this.clouds) {
+            let nr = (r + this.dirs[d][0] * (s % this.N) + this.N) % this.N;
+            let nc = (c + this.dirs[d][1] * (s % this.N) + this.N) % this.N;
+
+            grid.addWater(nr, nc);
+            movedClouds.push([nr, nc]);
+            isCloudIdx[nr][nc] = true;
+        }
+
+        // 물복사버그
+        this.castWaterCopySpell(movedClouds, grid);
+
+        // 새로운 구름 생성 (기존 구름 위치 제외)
+        this.createNewClouds(isCloudIdx, grid);
+    }
+
+    castWaterCopySpell(movedClouds, grid) {
+        for (const [r, c] of movedClouds) {
+            let count = 0;
+            const diags = [[-1, -1], [-1, 1], [1, 1], [1, -1]];
+            
+            for (const [dr, dc] of diags) {
+                const nr = r + dr;
+                const nc = c + dc;
+                if (grid.isValid(nr, nc) && grid.getWater(nr, nc) > 0) {
+                    count++;
+                }
+            }
+            grid.addWater(r, c, count);
+        }
+    }
+
+    createNewClouds(isCloudIdx, grid) {
+        const nextClouds = [];
+        for (let r = 0; r < this.N; r++) {
+            for (let c = 0; c < this.N; c++) {
+                if (grid.getWater(r, c) >= 2 && !isCloudIdx[r][c]) {
+                    grid.consumeWater(r, c);
+                    nextClouds.push([r, c]);
                 }
             }
         }
-        A[cloud[0]][cloud[1]] += cnt
-    }
-
-    // 구름 배열 수정
-    for (let r=0; r<N; ++r) {
-        for (let c=0; c<N; ++c) {
-            if (A[r][c] >= 2 && !isCloud[r][c]) {
-                A[r][c] -= 2
-                clouds.push([r, c])
-            }
-        }
+        this.clouds = nextClouds;
     }
 }
 
-let ans = 0
-for (const line of A) {
-  for (const water of line) ans += water
+function solve() {
+    const [N, M] = input[0].split(" ").map(Number);
+    const initialGridData = input.slice(1, N + 1).map(row => row.split(" ").map(Number));
+    const commands = input.slice(N + 1).map(row => row.split(" ").map(Number));
+
+    const grid = new Grid(N, initialGridData);
+    const cloudSystem = new CloudSystem(N);
+
+    for (const [d, s] of commands) {
+        cloudSystem.moveAndRain(d, s, grid);
+    }
+
+    console.log(grid.getTotalWater());
 }
-console.log(ans)
+
+solve();
