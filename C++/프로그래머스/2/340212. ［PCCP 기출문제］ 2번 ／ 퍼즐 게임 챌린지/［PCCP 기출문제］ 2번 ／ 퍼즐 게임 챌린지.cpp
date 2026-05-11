3f8f8aba9120f8b49e3439576d1cd3c68cb9ea8b
hyvnsevng <yh189487@gmail.com>
2026-05-11 13:37:18 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 5.71 ms, Memory: 23.5 MB -BaekjoonHub

--- C++/프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
+++ C++/프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.cpp
@@ -3,32 +3,35 @@
 
 using namespace std;
 
-bool check(int level, long long limit, int n, vector<int> &diffs, vector<int> &times) {
-    long long time = 0;
-    long long time_prev = 0;
-    for (int i = 0; i < n; ++i) {
+bool check_limit(vector<int> &diffs, vector<int> &times, int &n, long long &limit, int level) {
+    long long total_time = 0;
+    int time_prev = 0;
+    for (int i = 0; i < n; i++) {
         int diff = diffs[i];
         int time_cur = times[i];
         if (i > 0) time_prev = times[i-1];
-        if (diff <= level) time += time_cur;
-        else time += (diff - level) * (time_cur + time_prev) + time_cur;
-        if (time > limit) return false;
-        time_prev += time_cur;
-    }
+        
+        if (diff > level) total_time += (diff - level) * (time_cur + time_prev) + time_cur;
+        else total_time += time_cur;
+        
+   }
+    if (total_time > limit) return false;
     return true;
 }
 
 int solution(vector<int> diffs, vector<int> times, long long limit) {
+    int answer = 0;
     int n = diffs.size();
     int s = 1, e = 100000;
-    int answer = 0;
     while (s <= e) {
         int mid = (s + e) / 2;
-        if (check(mid, limit, n, diffs, times)) {
+        if (check_limit(diffs, times, n, limit, mid)) {
             answer = mid;
             e = mid - 1;
         }
-        else s = mid + 1;
+        else {
+            s = mid + 1;
+        }
     }
     return answer;
 }
