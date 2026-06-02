62063fd7c63bc1d5138b52339bb344b79a13a146
hyvnsevng <yh189487@gmail.com>
2026-06-02 18:49:32 +0900

    [level 2] Title: 비밀 코드 해독, Time: 85.86 ms, Memory: 11.3 MB -BaekjoonHub

--- Python3/프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ Python3/프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,9 +1,24 @@
 from itertools import combinations
 
+def sys_res(comb, query):
+    res = 0
+    query_set = set(query)
+    for num in comb:
+        if num in query_set:
+            res += 1
+    return res
+
+
+def backtracking(comb, q, ans, idx):
+    if idx == len(ans):
+        return 1
+    if sys_res(comb, q[idx]) != ans[idx]:
+        return 0
+    return backtracking(comb, q, ans, idx + 1)
+
+    
 def solution(n, q, ans):
-    codes = list(combinations(range(1, n + 1), 5))
-
-    for query, cnt in zip(q, ans):
-         codes = [code for code in codes if len(set(code) & set(query)) == cnt]
-
-    return len(codes)
+    answer = 0    
+    for comb in combinations([x for x in range(1, n + 1)], 5):
+        answer += backtracking(comb, q, ans, 0)
+    return answer
