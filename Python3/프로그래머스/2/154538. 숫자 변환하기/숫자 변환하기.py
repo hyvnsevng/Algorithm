af475da80c3e2056c879b8638e5420ce9b3a6100
hyvnsevng <yh189487@gmail.com>
2026-07-15 17:26:19 +0900

    [level 2] Title: 숫자 변환하기, Time: 193.28 ms, Memory: 34.4 MB -BaekjoonHub

--- Python3/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
+++ Python3/프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.py
@@ -1,19 +1,11 @@
 def solution(x, y, n):
-    dp = [-1] * (y + 1)
+    limit = 1000000
+    dp = [limit] * (3 * limit + 1)
     dp[x] = 0
     for i in range(x, y + 1):
         curr = dp[i]
-        if curr == -1:
-            continue
-        doubled, trippled, added = i * 2, i * 3, i + n
-        if doubled <= y:
-            if dp[doubled] == -1 or dp[doubled] > curr + 1:
-                dp[doubled] = curr + 1
-        if trippled <= y:
-            if dp[trippled] == -1 or dp[trippled] > curr + 1:
-                dp[trippled] = curr + 1
-        if added <= y:
-            if dp[added] == -1 or dp[added] > curr + 1:
-                dp[added] = curr + 1
+        dp[i * 2] = min(dp[i * 2], curr + 1)
+        dp[i * 3] = min(dp[i * 3], curr + 1)
+        dp[i + n] = min(dp[i + n], curr + 1)
     
-    return dp[y]
+    return -1 if dp[y] == limit else dp[y]
