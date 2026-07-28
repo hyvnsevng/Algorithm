ee7bb753bc598630844bd933206adc2c0346b825
hyvnsevng <yh189487@gmail.com>
2026-07-28 21:06:16 +0900

    [level 2] Title: 택배상자, Time: 133.71 ms, Memory: 47.7 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/131704. 택배상자/택배상자.py
@@ -0,0 +1,17 @@
+def solution(order):
+    answer = 0
+    n = order[-1]
+    max_box_num = 0
+    container = []
+    for box in order:
+        if container and container[-1] == box:
+            container.pop()
+            answer += 1
+        elif box > max_box_num:
+            container += [x for x in range(max_box_num + 1, box)]
+            max_box_num = box
+            answer += 1
+        else:
+            break
+        
+    return answer
