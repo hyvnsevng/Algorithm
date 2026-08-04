aaf3f048f8c6705b61d98ea6de27d8e581d184d1
hyvnsevng <yh189487@gmail.com>
2026-08-04 18:10:49 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.06 ms, Memory: 11.5 MB -BaekjoonHub

--- Python3/프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
+++ Python3/프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.py
@@ -1,5 +1,31 @@
+def go_up(floor):
+    diff = 10 - (floor % 10)
+    return ((floor + diff) // 10 , diff)
+
+
+def go_down(floor):
+    diff = floor % 10
+    return ((floor - diff) // 10, diff)
+
+
 def solution(storey):
-    if storey < 10:
-        return min(storey, 10 - storey + 1)
-    div, remainder = storey // 10, storey % 10
-    return min(solution(div) + remainder, solution(div + 1) + 10 - remainder)
+    answer = 100
+    stack = [(storey, 0)]
+    while stack:
+        floor, cnt = stack.pop()
+        
+        # 0 떼기
+        while floor % 10 == 0:
+            floor //= 10
+            
+        # 두 자릿수 이상이면 버튼 누르고 스택에 추가 
+        if floor > 10:            
+            n_floor, diff = go_down(floor)
+            stack.append((n_floor, cnt + diff))
+            n_floor, diff = go_up(floor)
+            stack.append((n_floor, cnt + diff))
+        # 한 자릿수면 최소값 갱신
+        else:
+            answer = min(answer, cnt + min(floor, 10 - floor + 1))  # 기존 최소값, 내려가는 버튼만 누르기, 10까지 올라갔다 내려가기
+        
+    return answer
